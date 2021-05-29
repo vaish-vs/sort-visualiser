416aaf3b9c4a46c78227a3fc78dd8f03a1ba366a
kcabhinav <kcabhinav42@gmail.com>
2021-05-29 15:03:11 +0530

    Renamed Variable

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 var board = document.getElementsByClassName("chars")[0];
-var array= new Array();
+var array = new Array();
 
-function appendthem(){
-    var input = document.getElementById("charInput").value; 
-    input = input.replace(/ /g,'')
-    console.log(input);
-    array = input.split(",");
+function appendThem() {
+  var input = document.getElementById("charInput").value;
+  input = input.replace(/ /g, "");
+  console.log(input);
+  array = input.split(",");
 
-    while (board.firstChild) {
-        board.removeChild(board.lastChild);
-    }
+  while (board.firstChild) {
+    board.removeChild(board.lastChild);
+  }
 
-    for(var i=0; i<array.length; i++){
-        var elem = document.createElement("p");
-        elem.style.marginLeft="20px";
-        var textnode  = document.createTextNode(array[i]);
-        elem.appendChild(textnode);
-        board.appendChild(elem);    
-    }
+  for (var i = 0; i < array.length; i++) {
+    var elem = document.createElement("p");
+    elem.style.marginLeft = "20px";
+    var textnode = document.createTextNode(array[i]);
+    elem.appendChild(textnode);
+    board.appendChild(elem);
+  }
 
-    
-        bubbleSort();
-    
- 
+  bubbleSort();
 }
-
