a9bde0ae3066bf229d658df6fff73b4a01b7407f
kcabhinav <kcabhinav42@gmail.com>
2021-05-29 18:22:27 +0530

    Fixed Insertion Sort

--- algorithms/insertion.js
+++ algorithms/insertion.js
@@ -1,18 +1,14 @@
 var board = document.getElementsByClassName("chars")[0];
-var isSwapped = false;
 
 function insertionSort() {
   var key, i, j;
-  for (i = 1; i < array.length; i++) {
-    isSwapped = false;
-    key = Number(board.children[i].innerHTML);
+  for (i = 1; i < array.len; i++) {
+    key = board.children[i].innerHTML;
     j = i - 1;
-    while (j >= 0 && key > Number(board.children[j].innerHTML)) {
+    while (j >= 0 && Number(board.children[j].innerHTML) > Number(key)) {
       board.children[j + 1].innerHTML = board.children[j].innerHTML;
-      j--;
-      isSwapped = true;
+      j = j - 1;
     }
     board.children[j + 1].innerHTML = key;
-    if (!isSwapped) break;
   }
 }
