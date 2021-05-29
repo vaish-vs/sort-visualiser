4a688c231e638e35b39ed796618e98da0e20c3f6
unknown <vaishnav.vsft@gmail.com>
2021-05-29 20:42:57 +0530

    Added random array generator

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ var board = document.getElementsByClassName("chars")[0];
 var array = new Array();
 var algo = document.getElementsByClassName("algorithm"); //Gets all elements with class name "Algorithm". Algo will be an array.
 
+
 function appendThem() {
+
+  //Remove white spaces and split the values wherever  comma occurs
   var input = document.getElementById("charInput").value;
   input = input.replace(/ /g, "");
   array = input.split(",");
 
+  //Remove any existing children from old data when new data is entered.
   while (board.firstChild) {
     board.removeChild(board.lastChild);
   }
 
+
+  //Append the data from the input array as children of div
   for (var i = 0; i < array.length; i++) {
     var elem = document.createElement("p");
     elem.style.marginLeft = "20px";
@@ -19,6 +25,8 @@ function appendThem() {
     board.appendChild(elem);
   }
 
+
+  //Checking for which algorithm is selected in the Radio Buttons
   if (algo[0].checked) {
     bubbleSort();
   } else if (algo[1].checked) {
@@ -30,4 +38,10 @@ function appendThem() {
   } else {
     console.log("Not an option!");
   }
+}
+
+
+function randomArray(){
+  var randomArray = Array(8).fill().map( () => Math.round( Math.random() * 10 ) );
+  document.getElementById("charInput").value = randomArray;
 }
